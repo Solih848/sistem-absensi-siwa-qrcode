45d85f47cdd96226c7dbdc00d6d709c70fe61f78
Solih848 <solihudin848@gmail.com>
2024-09-03 06:40:36 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,11 @@
 <?php
-$servername = "localhost";  // Nama host database
-$username = "root";         // Username database
-$password = "";             // Password database (kosong jika tidak ada)
-$database = "absensi";   // Nama database
+$servername = "localhost";  
+$username = "root";         
+$password = "";            
+$database = "absensi";  
 
-// Membuat koneksi
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Mengecek koneksi
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
+// if (!$conn) {
+//     die("Koneksi gagal: " . mysqli_connect_error());
+// }
